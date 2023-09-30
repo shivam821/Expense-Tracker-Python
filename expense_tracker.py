e49ac4f202544ca32331ffde7cdc494534c71378
shivam821 <73484687+shivam821@users.noreply.github.com>
2023-09-30 17:32:34 +0530

    Initial commit

---
+++ expense_tracker.py
@@ -0,0 +1,26 @@
+def main():
+    print("Running Expense Tracker")
+    #Get user to input the expense
+    get_user_expense()
+
+    #Write expense to a file 
+    write_to_file() 
+
+    #Read the file and summarize the expense
+    summarize_expense()
+    
+
+def get_user_expense():
+    print("Getting User Expense")
+    
+
+def write_to_file():
+    print("Writing to File")
+    
+
+def summarize_expense():
+    print("Summarizing Expense")
+    
+
+if __name__ == "__main__":
+    main()
